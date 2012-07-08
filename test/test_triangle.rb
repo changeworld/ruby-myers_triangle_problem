c9b479419579e336fa96341bb6eda0f88099b0a0
changeworld <changesworlds@gmail.com>
2012-07-08 10:02:01 +0900

    作麼生

--- test/test_triangle.rb
+++ test/test_triangle.rb
@@ -9,4 +9,12 @@ class TestTriangle < Test::Unit::TestCase
     @triangle.c = 1
     assert @triangle.is_equilteral_triangle?, 'should be passed.'
   end
+
+  def test_not_triangle_0_0_0
+    @triangle = Triangle.new
+    @triangle.a = 0
+    @triangle.b = 0
+    @triangle.c = 0
+    assert !@triangle.is_equilteral_triangle?, 'not triangle!'
+  end
 end
