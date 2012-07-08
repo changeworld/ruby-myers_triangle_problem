08e4a4eddaf5407f01cb634f883b8e7d9e0f034e
changeworld <changesworlds@gmail.com>
2012-07-08 10:09:45 +0900

    Refactoring done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,17 @@
 class Triangle
   attr_accessor :a, :b, :c
 
+  def is_triangle?
+    @a > 0 && @b > 0 && @c > 0
+  end
+
   def is_equilteral_triangle?
-    @a > 0 && @b > 0 && @c > 0 &&
+    is_triangle? &&
       @a == @b && @b == @c && @c == @a
   end
 
   def is_isoscales_triangle?
-    @a > 0 && @b > 0 && @c > 0 &&
+    is_triangle? &&
       (@a == @b || @b == @c || @c == @a)
   end
 end
