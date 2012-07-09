de1b860c21bfe02c297d4b9e037f9dad58933ee3
changeworld <changesworlds@gmail.com>
2012-07-09 20:18:31 +0900

    require の記述を変更

--- test/test_triangle.rb
+++ test/test_triangle.rb
@@ -1,7 +1,5 @@
-$:.unshift File.join(File.dirname(__FILE__),'..','lib')
-
 require 'test/unit'
-require 'triangle'
+require './lib/triangle'
 
 class TestTriangle < Test::Unit::TestCase
   def setup
